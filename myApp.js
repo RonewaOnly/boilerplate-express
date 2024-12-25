adf64276a736ec664e9ee957501c5cb7dd41ca88
Ronewa Maselesele <idontgiveaf31@gmail.com>
2024-12-25 10:51:31 +0000

    include the http that will display the console and the default one

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 let express = require('express');
 let app = express();
 
+// Log "Hello World" directly when the file runs
+console.log("Hello World");
 
-app.get('/',(req,res)=>{
-    console.log("Hello World");
-})
+app.get('/', (req, res) => {
+    res.send("Hello World"); // This sends "Hello World" as a response when the root route is accessed
+});
+
 
 
 
