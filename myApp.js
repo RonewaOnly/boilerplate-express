66b273b150ffec1a024e0f239642e136180f6d62
Ronewa Maselesele <idontgiveaf31@gmail.com>
2024-12-25 10:59:53 +0000

    `Added static file serving for views and changed root route to serve index.html`

--- myApp.js
+++ myApp.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let app = express();
 
+app.use('/views', express.static(__dirname + '/views'));
 // Log "Hello World" directly when the file runs
 console.log("Hello World");
 
 app.get('/', (req, res) => {
-    res.send("Hello Express"); // This sends "Hello World" as a response when the root route is accessed
+    //res.send("Hello Express"); // This sends "Hello World" as a response when the root route is accessed
+
+    // Send the file at the given path
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
