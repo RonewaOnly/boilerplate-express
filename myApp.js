4eb33fccf66a8835e1e4446da93d4bc8adfa930c
Ronewa Maselesele <idontgiveaf31@gmail.com>
2024-12-25 11:16:52 +0000

    `Removed unnecessary variable assignment for dotenv config and removed empty line in '/json' route`

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let process = require('dotenv').config();
+require('dotenv').config(); 
 
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/views', express.static(__dirname + '/views'));
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 app.get('/json', (req, res) => {
-    
+
     if(process.env.MESSAGE_STYLE === 'uppercase'){
         res.json({"message": "HELLO JSON"});
     }
